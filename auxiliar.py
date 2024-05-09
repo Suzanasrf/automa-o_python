279752489ff9abe598af92323517c7c92d35b868
Suzana Fontes <suzanarfontes@gmail.com>
2024-05-09 08:28:24 -0300

    codigo auxiliar para pegar posição do mouse

---
+++ auxiliar.py
@@ -0,0 +1,10 @@
+import pyautogui as py
+import time 
+
+# Utilizando comando position
+
+
+# Utilizando o comando sleep para dar tempo de colocar o mouse no devido local
+
+time.sleep(4)
+print(py.position())
